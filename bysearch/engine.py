9ebff25bf8a49d1a5922b40f2494237517fa7fcf
tiholad <tiholad@gmail.com>
2023-09-29 21:42:36 +0300

    Engine type hints

--- bysearch/engine.py
+++ bysearch/engine.py
@@ -1,13 +1,17 @@
+from typing import Optional
 import numpy as np
+from numpy import float64
+from numpy.typing import NDArray
 from transformers import AutoTokenizer
-from datasets import load_from_disk
+from datasets import load_from_disk, Dataset
 import onnxruntime as ort
+from pandas import DataFrame
 
 from .backends import LocalBackend, PineconBackend, ChromaBackend
 
 
 class BySearch:
-    def get_embeddings(self, text_list):
+    def get_embeddings(self, text_list: list[str]) -> NDArray[np.float64]:
         encoded_input = self.tokenizer(
             text_list, padding='max_length', truncation=True, return_tensors="np", max_length=64, return_token_type_ids=False,
         )
@@ -15,7 +19,7 @@ class BySearch:
         model_output = self.session.run(None, input_feed=dict(encoded_input))
         return model_output[0][:, 0]
     
-    def load_dataset(self, dataset=None, path=None, compute_embeddings=False, batch_size=2):
+    def load_dataset(self, dataset: Optional[Dataset] = None, path: Optional[str] = None, compute_embeddings: bool = False, batch_size: int = 2) -> Dataset:
         if path is not None:
             dataset = load_from_disk(path)
         if compute_embeddings:
@@ -26,7 +30,7 @@ class BySearch:
             )
         return dataset
 
-    def __init__(self, dataset=None, path=None, compute_embeddings=False, tokenizer_checkpoint="KoichiYasuoka/roberta-small-belarusian", model_path='onnx\\by-model.onnx', backend='local', **kwargs):
+    def __init__(self, dataset: Optional[Dataset] = None, path: Optional[str] = None, compute_embeddings: bool = False, tokenizer_checkpoint: str = "KoichiYasuoka/roberta-small-belarusian", model_path: str = 'onnx\\by-model.onnx', backend: str = 'local', **kwargs) -> None:
         self.tokenizer  = AutoTokenizer.from_pretrained(tokenizer_checkpoint)
         self.session = ort.InferenceSession(model_path, providers=['CPUExecutionProvider'])
         self.dataset = self.load_dataset(dataset, path, compute_embeddings)
@@ -37,10 +41,10 @@ class BySearch:
         if backend == 'chroma':
             self.backend = ChromaBackend(self.dataset, **kwargs)
 
-    def add_data(self, dataset=None, path=None, compute_embeddings=False):
+    def add_data(self, dataset: Optional[Dataset] = None, path: str = None, compute_embeddings: bool = False) -> None:
         dataset = self.load_dataset(dataset, path, compute_embeddings)
         self.backend.add_data(dataset)
 
-    def search(self, prompt, k=5, verbose=True):
+    def search(self, prompt: str, k: int = 5, verbose: bool = True) -> DataFrame:
         embedding = self.get_embeddings([prompt])
         return self.backend.search(embedding, k, verbose)
